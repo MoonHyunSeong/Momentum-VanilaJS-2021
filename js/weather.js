c714c41eac5211147b4eb32dc0d69b3a1af7bc41
MoonHyunSeong <gustjd617@gmail.com>
2022-06-22 16:25:50 +0900

    upload

--- js/weather.js
+++ js/weather.js
@@ -2,14 +2,14 @@ function onGeoOk(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
     const API_KEY = "bbd19049276c2b025fe1752961c2d158";
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
     fetch(url)
     .then((response) => response.json())
     .then((data) => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
+        const weather = document.querySelector(".top_bar #info span");
+        const city = document.querySelector(".top_bar #location span");
         city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main}`;
+        weather.innerText = `${data.main.temp}℃ ${data.weather[0].main}`;
     });
 }
 function onGeoError(){
